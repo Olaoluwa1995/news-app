1b1245fc96be5eaa6cc0114cd95b03ba7429e687
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-14 21:53:50 +0100

    Modified app routes

--- lib/constants/app-routes.dart
+++ lib/constants/app-routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/views/news.dart';
 import 'package:news_app/views/signin.dart';
 import 'package:news_app/views/signup.dart';
 
@@ -6,12 +7,12 @@ class Routes {
   static const SIGN_UP_SCREEN_ROUTE = '/';
   static const SIGN_IN_SCREEN_ROUTE = '/sign-in';
   static const NEWS_SCREEN_ROUTE = '/news';
-  // static const FAVORITE_SCREEN_ROUTE = '/favorite';
 
   static Map<String, WidgetBuilder> routes() {
     return <String, WidgetBuilder>{
       SIGN_UP_SCREEN_ROUTE: (context) => SignUpScreen(),
       SIGN_IN_SCREEN_ROUTE: (context) => SignInScreen(),
+      NEWS_SCREEN_ROUTE: (context) => NewsScreen(),
     };
   }
 }
