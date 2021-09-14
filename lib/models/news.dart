ddb3b5007c95c86e820f5c8a045c80aafda7aeec
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-14 21:52:34 +0100

    Created news model

---
+++ lib/models/news.dart
@@ -0,0 +1,13 @@
+class News {
+  final int id;
+  final String title;
+  final String summary;
+  final String published;
+
+  News({
+    required this.id,
+    required this.title,
+    required this.summary,
+    required this.published,
+  });
+}
