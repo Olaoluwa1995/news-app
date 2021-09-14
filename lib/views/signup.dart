12c840db9d96889687e485eca43f0f5231d0d6b7
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-14 16:58:54 +0100

    Updated with functionality

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -1,6 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:news_app/constants/app-api.dart';
+import 'package:news_app/constants/app-routes.dart';
 import 'package:news_app/constants/widgets/custom-button.dart';
 import 'package:news_app/constants/widgets/custom-input.dart';
+import 'package:news_app/constants/widgets/error.dart';
+import 'package:news_app/constants/widgets/loader.dart';
 import 'package:news_app/views/widgets/auth_layout.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -46,7 +53,55 @@ class _SignUpScreenState extends State<SignUpScreen> {
     return null;
   }
 
-  
+  void _showLoader() {
+    showDialog(
+      context: context,
+      builder: (ctx) => Loader(),
+    );
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => GestureDetector(
+        child: ErrorView(message),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+
+
+  Future<void> _register() async {
+    if (!_registerFormKey.currentState!.validate()) {
+      // Invalid !
+      return;
+    }
+    _registerFormKey.currentState!.save();
+    print(_registerData);
+
+    if (_registerData['password'] !=
+        _registerData['passwordConf']) {
+      _showErrorDialog('Passwords don\'t match!');
+    } else {
+      _showLoader();
+      http.Response response = await http.post(
+      Uri.parse('${AppApi.signupUrl}'),
+      body: json.encode(_registerData),
+      headers: {'Content-Type': 'application/json'},
+      );
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      if(responseData.containsValue("You are regestered,You can login now.")) {
+        Navigator.of(context).pop();
+        // Navigator.of(context).pushReplacementNamed(routeName);
+      } else {
+        Navigator.of(context).pop();
+        _showErrorDialog(responseData['Success']);
+      }
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -112,7 +167,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 ),
                 CustomButton(
                   text: 'Sign Up',
-                  onpressed: () {},
+                  onpressed: _register,
                 ),
               ],
             ),
@@ -128,7 +183,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
             width: size.width * 0.01,
           ),
           GestureDetector(
-            onTap: () {},
+            onTap: () => Navigator.of(context).pushReplacementNamed(Routes.SIGN_IN_SCREEN_ROUTE),
             child: Text('Sign in', style: TextStyle(
               color: Colors.orange
             ),),
