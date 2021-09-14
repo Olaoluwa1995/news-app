2f3066089ad8bbb4cb8425ae81cfdd307548ec04
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-14 14:18:41 +0100

    Added color class

---
+++ lib/constants/app-colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+/// App Colors Class - Resource class for storing app level color constants
+class AppColors {
+  static const Color BACKGROUND_COLOR = Color(0xFFE5E5E5);
+  static const Color backgroundColor = Color(0xFFEEEEEE);
+  static const Color primaryColor = Color(0xFF59BCD0);
+}
