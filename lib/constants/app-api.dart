95d059b90d79b9ee6fc458cbc5795e723d24c0bf
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-14 14:18:31 +0100

    Added api class

---
+++ lib/constants/app-api.dart
@@ -0,0 +1,8 @@
+class AppApi {
+  // AUTH
+  static const String signupUrl = "https://nodejs-register-login-app.herokuapp.com";
+  static const String signinUrl = "https://nodejs-register-login-app.herokuapp.com/login";
+
+  // NEWS
+  static const String newsUrl = "https://api.first.org/data/v1/news";
+}
